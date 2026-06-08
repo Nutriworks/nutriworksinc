5134f8cce9eba7e2b3f026cc1904518e49de8d8c
jackaogle <55500944+jackaogle@users.noreply.github.com>
2026-06-08 14:57:13 -0500

    Add files via upload

---
+++ code-inject-site-LIVE.js
@@ -0,0 +1,12 @@
+/* 
+  hyp.com 
+  LU  5 SEP 2019
+*/
+
+$(document).ready(function($) {
+  biolinks();
+});
+
+function biolinks(){
+  $('#testimonials-section-copy .trigger.open.bio').text('FULL TESTIMONIAL');
+}
